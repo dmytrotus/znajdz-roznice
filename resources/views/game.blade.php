6ac0567d5a1002490e61ace62f0457b8f65c6de2
Dmytro Tus <dmytrotus@gmail.com>
2019-09-03 14:55:33 +0200

    added frontend

---
+++ resources/views/game.blade.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="pl">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+    <style>
+      .navbar{
+        justify-content: space-between;
+      }
+      .row img{
+          width: 50%;
+      }
+      @media (max-width: 400px){
+        .row img{
+          width: 100%;
+        }
+        .btn{
+          margin: 0 auto;
+        }
+      }
+      li.row{
+        margin: 0 auto;
+      }
+      li.row p{
+        float: left;
+        margin: 0 30px;
+      }
+      li.row p span{
+        font-weight: bolder;
+      }
+
+    </style>
+
+    <title>Znajdź różnicę</title>
+  </head>
+  <body>
+    <h1 class="card-header text-center">
+    Znajdź różnicę
+  </h1>
+
+
+    <div class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+      <a class="navbar-brand" href="#">Znaleziono 2 z 5</a>
+      <a class="navbar-brand" href="#">Czas: 00:00</a>
+      <a class="btn btn-success" href="#">OD NOWA</a>
+      </div>
+    </div>
+
+
+    <div class="row">
+      <img src="images/a.png" alt="" class="img-responsive">
+      <img src="images/b.jpg" alt="" class="img-responsive">
+    </div>
+
+
+    <div class="card" style="width: 100%;">
+  <h2 class="card-header text-center">
+    Wyniki
+  </h2>
+  <ul class="container list-group list-group-flush">
+    <li class="list-group-item row">
+    <p><span>Nick:</span> Agnieszka</p>
+    <p><span>Czas:</span> 00:45</p>
+    </li>
+
+    <li class="list-group-item row">
+    <p><span>Nick:</span> Agnieszka</p>
+    <p><span>Czas:</span> 00:45</p>
+    </li>
+
+  </ul>
+</div>
+    
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
