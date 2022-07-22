dedc34206f1d2b6ee87d7555f29e07e6767eb904
Dmytro Tus <dmytrotus@gmail.com>
2022-07-22 20:20:49 +0100

    update libraries and small fixes

--- app/Http/Controllers/GamerController.php
+++ app/Http/Controllers/GamerController.php
@@ -6,31 +6,30 @@ use Illuminate\Http\Request;
 use App\Gamer;
 use Session;
 
-
 class GamerController extends Controller
 {
     public function index()
     {
-    	return view('game')
-    	->with('gamers', Gamer::all());
+        return view('game')
+        ->with('gamers', Gamer::all());
     }
 
     public function savedata()
     {
-    	$r = request();
+        $r = request();
 
-    	/*$this->validate($r, [
+        /*$this->validate($r, [
     		'nick' => 'required|min:3',
     		'time' => 'required'
     	]);*/
 
-    	$gamer = Gamer::create([
-    		'nick' => $r->nick,
-    		'time' => $r->time
-    	]);
+        $gamer = Gamer::create([
+            'nick' => $r->nick,
+            'time' => $r->time
+        ]);
 
-    	Session::flash('success', 'Dziękuję. Zobacz swoją pozycję w rankingu na dole');
+        Session::flash('success', 'Dziękuję. Zobacz swoją pozycję w rankingu na dole');
 
-    	return back();
+        return redirect()->back();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/game',  'GamerController@index');
+Route::get('/', 'GamerController@index');
 
 Route::post('/savedata', 'GamerController@savedata');
